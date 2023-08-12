2f2d9f1b5b2d97bd07690626fe9884ab3e90b979
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-08-12 19:10:45 +0300

    passed all tests

--- lib/ipdb_is_awesome.py
+++ lib/ipdb_is_awesome.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python3
-
 import ipdb
+
 
 def tracing_the_function():
     inside_the_function = "We're inside the function"
     print(inside_the_function)
     print("We're about to stop because of ipdb!")
+    myString = "abdi was here"
     ipdb.set_trace()
-    this_variable_hasnt_been_interpreted_yet = \
+    this_variable_hasnt_been_interpreted_yet = (
         "The program froze before it could read me!"
+    )
     print(this_variable_hasnt_been_interpreted_yet)
+
 
 tracing_the_function()
